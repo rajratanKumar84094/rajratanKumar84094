e90098a6d429a036e8a7708b0eacc296749817ab
rajratanKumar84094 <145573775+rajratanKumar84094@users.noreply.github.com>
2023-09-24 20:45:14 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+#SIMPLE CALCULATOR CODE
+
+First= input( "enter the first number:")
+
+operator= input("enter operator(+,-,*,/):")
+
+Second= input( "enter the second number:")
+
+if operator=="+":
+   print(int(First)+int(Second))
+
+elif operator=="-" :
+     print(int(First)-int(Second))
+
+elif operator=="*":
+     print(int(First)*int(Second))
+
+
+
+elif operator=="/":
+     print(int(First)/int(Second))
+
+
+
+else:
+    print("invalid operation")
